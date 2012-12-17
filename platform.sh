085e97ded31261f225088f76466015336506df65
Seth Raphael <seth@bu.mp>
2012-12-17 13:22:32 -0800

    fix framework extra dash

--- platform.sh
+++ platform.sh
@@ -3,7 +3,7 @@ platform=`uname`
 EXTRA_LDFLAGS=""
 if test $platform == "Darwin"; then 
     echo " ---> Configured for Darwin">&2;
-    EXTRA_LDFLAGS="-framework CoreServices -framework -CoreFoundation $EXTRA_LDFLAGS"
+    EXTRA_LDFLAGS="-framework CoreServices -framework CoreFoundation $EXTRA_LDFLAGS"
 elif test $platform == "Linux"; then
     echo " ---> Configured for Linux">&2;
 fi
