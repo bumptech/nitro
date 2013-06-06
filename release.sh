6cf141c45199b001aface242aa0c4dd9f5319e8d
Jamie Turner <jamie@jamwt.com>
2013-06-06 16:01:54 -0500

    Get some safety on release script.

--- release.sh
+++ release.sh
@@ -8,14 +8,14 @@ assert len(sys.argv) == 3, "need CHANGESET VERSION"
 cset = sys.argv[1]
 version = sys.argv[2]
 
-os.system("mkdir -p downloads")
+assert not os.system("mkdir -p downloads")
 os.chdir("downloads")
-os.system("git clone ../ nitro-" + version)
+assert not os.system("git clone ../ nitro-" + version)
 os.chdir("nitro-" + version)
-os.system("git checkout " + cset)
-os.system("rm -rf .git")
+assert not os.system("git checkout " + cset)
+assert not os.system("rm -rf .git")
 os.chdir("..")
-os.system("tar cf nitro-%s.tar nitro-%s" % (version, version))
-os.system("gzip nitro-%s.tar" % version)
+assert not os.system("tar cf nitro-%s.tar nitro-%s" % (version, version))
+assert not os.system("gzip nitro-%s.tar" % version)
 hash = hashlib.sha1(open("nitro-%s.tar.gz" % version).read())
 open("nitro-%s.tar.gz.sha1" % version, 'wb').write(hash.hexdigest())
